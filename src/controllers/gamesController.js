e100e16fafdf499fc2b1a05a96cd369a95b87e16
devmatheusdias <dev.matheusdias@gmail.com>
2023-05-13 15:20:42 -0300

    Controller games pronto

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,17 +1,33 @@
+import { db } from '../database/database.connection.js'
 
-// import {db} from '../database/database.js'
 
-
-export async function listGames(req, res){ 
-    res.send('ok');
+export async function listGames(req, res) {
+    try {
+        const games = await db.query("SELECT * FROM games");
+        res.send(games.rows);
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 }
 
-export async function insertGame(req,res){
-   const {name, image, stockTotal, pricePerDay} = req.body;
+export async function insertGame(req, res) {
+    const { name, image, stockTotal, pricePerDay } = req.body;
+    
+    try {
+        const games = await db.query("SELECT (name) FROM games");
 
-   try {
-        res.send(`name: ${name} | image: ${image} | stockTotal: ${stockTotal} | pricePerDay: ${pricePerDay}`)
-   } catch (err) {
+        const gameExist = games.rows.find(game => game.name === name)
+
+        if(gameExist) return res.status(409).send('Este jogo já existe');
+
+        db.query(
+            `INSERT INTO games (name, image, "stockTotal", "pricePerDay")
+             VALUES ('${name}', '${image}', '${stockTotal}', '${pricePerDay}')`, 
+        )
+
+        return res.status(201).send('ok!')
+
+    } catch (err) {
         res.send(err.message)
-   }
+    }
 }
